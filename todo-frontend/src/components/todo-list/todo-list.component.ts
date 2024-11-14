ecb2b6ef4c385d6d6ee3e6dc2bf16dfddd28105d
Allan Corriere <allan.corriere@gmail.com>
2024-11-14 16:18:49 +0100

    Click to edit a todo

--- todo-frontend/src/components/todo-list/todo-list.component.ts
+++ todo-frontend/src/components/todo-list/todo-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { TodoListItemComponent } from '../todo-list-item/todo-list-item.component';
 import { Todo } from '../../entities/todo.entity';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'todo-list',
   standalone: true,
-  imports: [TodoListItemComponent],
+  imports: [TodoListItemComponent, RouterLink],
   templateUrl: './todo-list.component.html',
 })
 export class TodoListComponent {
