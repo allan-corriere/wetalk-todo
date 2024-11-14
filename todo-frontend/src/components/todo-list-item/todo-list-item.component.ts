78456767ed51b49aa417efa8093396816d3f3621
Allan Corriere <allan.corriere@gmail.com>
2024-11-14 20:59:25 +0100

    Delete todo

--- todo-frontend/src/components/todo-list-item/todo-list-item.component.ts
+++ todo-frontend/src/components/todo-list-item/todo-list-item.component.ts
@@ -1,11 +1,33 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'todo-list-item',
   standalone: true,
+  imports: [FontAwesomeModule, RouterLink],
   templateUrl: './todo-list-item.component.html',
 })
 export class TodoListItemComponent {
   @Input() todoTitle = '';
   @Input() todoDescription = '';
+  @Input() todoUuid = '';
+
+  faTrash = faTrash;
+
+  constructor(private todoService: TodoService) {}
+
+  showModal() {
+    if (document) {
+      (document.getElementById(`modal_${this.todoUuid}`) as HTMLFormElement)[
+        'showModal'
+      ]();
+    }
+  }
+
+  deleteTodo() {
+    this.todoService.deleteTodo(this.todoUuid).subscribe((todo) => {});
+  }
 }
--- todo-frontend/src/components/todo-list/todo-list.component.ts
+++ todo-frontend/src/components/todo-list/todo-list.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { TodoListItemComponent } from '../todo-list-item/todo-list-item.component';
 import { Todo } from '../../entities/todo.entity';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'todo-list',
   standalone: true,
-  imports: [TodoListItemComponent, RouterLink],
+  imports: [TodoListItemComponent],
   templateUrl: './todo-list.component.html',
 })
 export class TodoListComponent {
